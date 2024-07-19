75baa54f334cea2321018a6f8886b8a371ca675b
Kam Salisbury <29674098+kamsalisbury@users.noreply.github.com>
2024-07-19 12:27:02 -0400

    Update run-codellama.sh

--- run-codellama.sh
+++ run-codellama.sh
@@ -3,5 +3,6 @@
 # Estimated disk space 4.3GB
 # https://github.com/Intel-Media-SDK/MediaSDK/wiki/Running-on-GPU-under-docker
 # and sudo apt install intel-gpu-tools
-docker run -d --device /dev/dri -v ollama:/root.ollama --name ollama ollama/ollama
-docker exec -it ollama ollama run codellama
+docker run -m=13G --memory-swap 16G -d --device /dev/dri -v ollama:/root.ollama --name ollama ollama/ollama:latest
+sudo docker exec -it ollama ollama run phi3:mini
+sudo docker stop ollama
